d50d7d63202b361dd0296f22dd494587bf951f9c
Matthew Phillips <matthew@matthewphillips.info>
2017-01-21 09:49:15 -0500

    Use ES5 constructor for impl
    Even though using custom elements with ES5 is really hard, some might
    want to do it, so our stuff should be in ES5.

--- can-custom-elements.js
+++ can-custom-elements.js
@@ -3,44 +3,52 @@ var getChildNodes = require("can-util/dom/child-nodes/child-nodes");
 var nodeLists = require("can-view-nodelist");
 
 function CustomElement(BaseElement) {
-	return class extends BaseElement {
-		constructor(){
-			super();
+	function CanElement(){
+		var self = Reflect.construct(BaseElement, arguments, this.constructor);
 
-			this._rendered = false;
-			var Element = this.constructor;
-			if(Element.view) {
-				this.attachShadow({ mode: "open" });
-			}
+		self._rendered = false;
+		var Element = self.constructor;
+		if(Element.view) {
+			self.attachShadow({ mode: "open" });
 		}
 
-		connectedCallback(){
-			// What is the root from which we render?
-			var root = this.shadowRoot || this;
-
-			// We only want to render once
-			// but connectedCallback gets called any time the element is inserted
-			// which could be N number of times.
-			if(!this._rendered) {
-				var Element = this.constructor;
-
-				var frag = Element.view(this.viewModel);
-
-				// Append the resulting document fragment to the element
-				domMutate.appendChild.call(root, frag);
-				this._rendered = true;
-			}
-
-			this._nodeList = nodeLists.register([], null, true, false);
-			this._nodeList.expression = "<" + this.localName + ">";
-			// update the nodeList with the new children so the mapping gets applied
-			nodeLists.update(this._nodeList, getChildNodes(root));
-		}
-
-		disconnectedCallback(){
-			nodeLists.unregister(this._nodeList);
-		}
+		return self;
 	}
+
+	CanElement.prototype = Object.create(BaseElement.prototype);
+	CanElement.prototype.constructor = CanElement;
+
+	var proto = CanElement.prototype;
+
+	proto.connectedCallback = function(){
+		// What is the root from which we render?
+		var root = this.shadowRoot || this;
+
+		this._nodeList = nodeLists.register([], null, true, false);
+		this._nodeList.expression = "<" + this.localName + ">";
+
+		// We only want to render once
+		// but connectedCallback gets called any time the element is inserted
+		// which could be N number of times.
+		if(!this._rendered) {
+			var Element = this.constructor;
+
+			var frag = Element.view(this.viewModel, null, this._nodeList);
+
+			// Append the resulting document fragment to the element
+			domMutate.appendChild.call(root, frag);
+			this._rendered = true;
+		}
+
+		// update the nodeList with the new children so the mapping gets applied
+		nodeLists.update(this._nodeList, getChildNodes(root));
+	};
+
+	proto.disconnectedCallback = function(){
+		nodeLists.unregister(this._nodeList);
+	};
+
+	return CanElement;
 }
 
 exports = module.exports = CustomElement;
