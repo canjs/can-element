0cc28e7cb7a4aa991d90065533088034ff376a2e
Kevin Phillips <kevin@bitovi.com>
2019-05-02 09:34:45 -0500

    initial class api stuff

--- can-element.js
+++ can-element.js
@@ -1,76 +1,27 @@
-var assign = require("can-util/js/assign/assign");
-var domData = require("can-util/dom/data/data");
-var domMutate = require("can-util/dom/mutate/mutate");
-var getChildNodes = require("can-util/dom/child-nodes/child-nodes");
-var nodeLists = require("can-view-nodelist");
-var Scope = require("can-view-scope");
+var observe = require("can-observe");
+var canSymbol = require("can-symbol");
 
-function CustomElement(BaseElement) {
-	function CanElement(){
-		var self = Reflect.construct(BaseElement, arguments, this.constructor);
+var viewModelSymbol = canSymbol.for("can.viewModel");
+var proxyMetaSymbol = canSymbol.for("can.proxyMeta");
 
-		self._rendered = false;
-		var Element = self.constructor;
-		if(Element.view) {
-			self.attachShadow({ mode: "open" });
-		}
-
-		// Mark the element as its own viewModel for binding purposes
-		var existingViewModel = domData.get.call(self, "viewModel");
-		if(existingViewModel) {
-			assign(self, existingViewModel.get());
-		}
-		domData.set.call(self, "viewModel", self);
-
-		return self;
-	}
-
-	CanElement.prototype = Object.create(BaseElement.prototype);
-	CanElement.prototype.constructor = CanElement;
-
-	var proto = CanElement.prototype;
-
-	proto.connectedCallback = function(){
-		// What is the root from which we render?
-		var root = this.shadowRoot || this;
-
-		// We only want to render once but connectedCallback gets called
-		// any time the element is inserted which could be N number of times.
-		if(!this._rendered) {
-			// setup our nodeList
-			this._nodeList = nodeLists.register([], null, true, false);
-			this._nodeList.expression = "<" + this.localName + ">";
-
-			var Element = this.constructor;
-			var scope = new Scope(this);
-			var frag = Element.view(scope, null, this._nodeList);
-
-			// Append the resulting document fragment to the element
-			domMutate.appendChild.call(root, frag);
-			this._rendered = true;
-		}
-
-		// update the nodeList with the new children so the mapping gets applied
-		nodeLists.update(this._nodeList, getChildNodes(root));
+function observable(BaseElement = HTMLElement) {
+	var ChildElement = function ChildElement() {
+		return Reflect.construct(BaseElement, [], this.constructor);
 	};
 
-	proto.disconnectedCallback = function(){
-		nodeLists.unregister(this._nodeList);
-	};
+	ChildElement.prototype = observe(Object.create(BaseElement.prototype));
 
-	Object.defineProperty(CanElement, "observedAttributes", {
-		get: function(){
-			return this._observedAttributes || [];
+	// can-stache-bindings uses viewModel symbol
+	Object.defineProperty(ChildElement.prototype, viewModelSymbol, {
+		get() {
+			var metadata = this[proxyMetaSymbol];
+			return metadata && metadata.target;
 		}
 	});
 
-	proto.attributeChangedCallback = function(attr, oldVal, newVal){
-		this[attr] = newVal;
-	};
-
-	return CanElement;
+	return ChildElement;
 }
 
-exports = module.exports = CustomElement;
+var CanElement = observable(HTMLElement);
 
-exports.Element = CustomElement(HTMLElement);
+module.exports = CanElement;
